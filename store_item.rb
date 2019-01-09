102ea6c19a577da217abe65e1c0b189988966f64
Tacoholic <solischristian@gmail.com>
2019-01-08 18:48:18 -0600

    answers to store_item

--- store_item.rb
+++ store_item.rb
@@ -2,5 +2,5 @@ item_1 = {:fruit => "papaya", :price => 2, :color => "green"}
 item_2 = {fruit: "banana", price: 1, color: "yellow"}
 item_3 = {:fruit => "apple", :price => 1, :color => "red"}
 
-puts "#{ item_1[:fruit] } is #{ item_1[:price] } dollars and it's  #{ item_1[:color]}"
-puts "#{ item_2[fruit:]} is #{ item_2[price:]} #{item_2[color:]}"
+puts "#{ item_1[:fruit] } is #{ item_1[:price] } dollars and it's  #{ item_1[:color] }"
+puts "#{ item_2[:fruit] } is #{ item_2[:price] } #{ item_2[:color] }"
