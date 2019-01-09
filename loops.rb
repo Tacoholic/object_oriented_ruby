c17f8cf57ee5e4359683de19845a5ccd7c670438
Tacoholic <solischristian@gmail.com>
2019-01-08 18:16:51 -0600

    initial commit

---
+++ loops.rb
@@ -0,0 +1,6 @@
+count = 10 
+
+until count > 0 
+	puts "count"
+	count -= 1
+end 
---
+++ store_item.rb
@@ -0,0 +1,6 @@
+item_1 = {:fruit => "papaya", :price => 2, :color => "green"}
+item_2 = {fruit: "banana", price: 1, color: "yellow"}
+item_3 = {:fruit => "apple", :price => 1, :color => "red"}
+
+puts "#{ item_1[:fruit] } is #{ item_1[:price] } dollars and it's  #{ item_1[:color]}"
+puts "#{ item_2[fruit:]} is #{ item_2[price:]} #{item_2[color:]}"
